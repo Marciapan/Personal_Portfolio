317a9eb21209c0e4904a27a1c98ff0e0513f8445
Marciapan <122381666+Marciapan@users.noreply.github.com>
2023-11-23 11:44:40 +0100

    Small changes and footer
    Made a start on the footer for desktop. Also decided to make more sketches first

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 console.log("Dit is een test");
+
+
+// vars
+var work1 = document.querySelector("#first");
+
+// eventlisteners
+work1.addEventListener("click", testDit);
+
+// functions
+function testDit(){
+    console.log("Test voor JS");
+    work1.classList.toggle("yay");
+
+}
