6fa8ba6cbe4c3ca1f4970ed81a1430cb19565002
Marciapan <122381666+Marciapan@users.noreply.github.com>
2023-12-06 18:53:57 +0100

    start of desktop view omg
    lets keep this uppppppppp

--- js/script.js
+++ js/script.js
@@ -3,13 +3,32 @@ console.log("Dit is een test");
 
 // vars
 var work1 = document.querySelector("#first");
+var projectbutton1 = document.querySelector("#projectbutton1");
+var projectbutton2 = document.querySelector("#projectbutton2");
+var extraInfo1 = document.querySelector("#extraInfo1");
+var extraInfo2 = document.querySelector("#extraInfo2");
 
 // eventlisteners
-work1.addEventListener("click", testDit);
+projectbutton1.addEventListener("click", extraInfo1Toggle);
+projectbutton2.addEventListener("click", extraInfo2Toggle);
+
+console.log(projectbutton2);
+console.log(extraInfo2);
+
 
 // functions
 function testDit(){
     console.log("Test voor JS");
     work1.classList.toggle("yay");
 
+}
+
+function extraInfo1Toggle(){
+    console.log("Hiding extra info");
+    extraInfo1.classList.toggle("hidden");
+}
+
+function extraInfo2Toggle(){
+    console.log("Hiding or showing extra info");
+    extraInfo2.classList.toggle("hidden");
 }
